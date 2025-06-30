fe58fac75c7fa15e922ee24acf9582dc230fec88
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-06-29 23:52:11 -0300

    Created success route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import PaymentPage from "./pages/PaymentPage";
 
 function App() {
-  return <PaymentPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaymentPage />} />
+        <Route path="/success" element={<div><p>Pago exitoso!!! <span>Volver</span></p></div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
